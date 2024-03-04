bb63c6e9a113e6f3215e726e6a123f8e92c42d84
vladisluw <komkov222111@gmail.com>
2024-03-05 01:05:52 +0500

    comment fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,25 @@ import { API_URL } from "@/core/config/api.config";
 
 export default async function HomePage() {
 
-  const bannerRes = await fetch(API_URL + '/api/banners/?populate=image')
-  const { data: bannerData } = await bannerRes.json()
+  //const bannerRes = await fetch(API_URL + '/api/banners/?populate=image')
+  //const { data: bannerData } = await bannerRes.json()
 
-  const faqRes = await fetch(API_URL + '/api/questions')
-  const { data: faqData } = await faqRes.json()
+  //const faqRes = await fetch(API_URL + '/api/questions')
+  //const { data: faqData } = await faqRes.json()
 
-  const stagesRes = await fetch(API_URL + '/api/stages')
-  const { data: stagesData } = await stagesRes.json()
+  //const stagesRes = await fetch(API_URL + '/api/stages')
+  //const { data: stagesData } = await stagesRes.json()
 
   return (
     <>
-      <Banner data={bannerData} />
+      {/*<Banner data={bannerData} />
       <hr className="hr" />
       <FAQ header={'Частые вопросы'} data={faqData} />
-      <hr className="hr" />
+      <hr className="hr" />*/}
       {/*<Catalog header={'Каталог услуг'} data={} />*/}
-      <hr className="hr" />
+      {/*<hr className="hr" />
       <Stages data={stagesData} header={'Как оформить заказ?'} />
-      <hr className="hr" />
+      <hr className="hr" />*/}
     </>
   );
 }
