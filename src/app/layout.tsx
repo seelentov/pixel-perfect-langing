d1e9a324ef6813821ef3246c2ef9e53a4bc1abcb
vladisluw <komkov222111@gmail.com>
2024-03-13 03:36:10 +0500

    add yandex.metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,26 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={inter.className}>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(96723379, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/12345678" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
         <Header />
         <main className="wrapper">
           {children}
