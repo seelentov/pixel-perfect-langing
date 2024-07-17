8da9eabaf76eb2235e7ec3e51aef2dff82267ef2
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-07-17 18:08:46 +0500

    Update api.config.ts
    Switch tg dialog

--- src/core/config/api.config.ts
+++ src/core/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_URL = process.env.API_URL || 'https://pixellperfect.ru'
 export const API_TOKEN = process.env.API_TOKEN
 export const TELEGRAM_API_KEY = "6454568318:AAFIqVdc_oB71z24yogZOpY5fSUmmLh_q5o";
-export const TELEGRAM_CHAT_ID = 816233444;
+export const TELEGRAM_CHAT_ID = -4200885676;
