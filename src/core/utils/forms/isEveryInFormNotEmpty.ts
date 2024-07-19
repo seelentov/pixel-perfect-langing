056bbbdec31c6123d9d5de22bdb61c8b1ba18462
vladisluw <54843319+seelentov@users.noreply.github.com>
2024-07-19 12:24:37 +0500

    Update isEveryInFormNotEmpty.ts

--- src/core/utils/forms/isEveryInFormNotEmpty.ts
+++ src/core/utils/forms/isEveryInFormNotEmpty.ts
@@ -1,5 +1,8 @@
 export const isEveryInFormNotEmpty = (formData: {[key: string]: string}) => {
-    const result = Object.values(formData).every(input => (input && input?.length > 0) || true)
+    const result = Object.values(formData).every(input => {
+        const trimmedInput = input.trim()
+        return trimmedInput != "" && trimmedInput?.length > 0
+    })
     console.log(result)
     return result
 }
